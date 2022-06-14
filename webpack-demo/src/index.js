537a2dedb65b788221f315809d778f4b97978f12
Talha <talha.b.future@gmail.com>
2022-06-14 11:48:15 +0300

    .

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,14 +1,6 @@
 import _ from 'lodash';
 
-function component() {
-    const element = document.createElement('div');
-  
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-//   document.body.appendChild(component());
+
+
   console.log(`I command thee KNEEL `);
   
