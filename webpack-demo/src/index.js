f54b824ddc30aaf388347790269b72a053886e44
Talha <talha.b.future@gmail.com>
2022-06-14 10:14:16 +0300

    started from the beggining again

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,13 +1,14 @@
-import _ from 'lodash'; 
+import _ from 'lodash';
 
- function component() {
-   const element = document.createElement('div');
-
-   // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-   console.log(`I command thee kneel`)
-   return element;
- }
-
- document.body.appendChild(component());
+function component() {
+    const element = document.createElement('div');
+  
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+//   document.body.appendChild(component());
+  console.log(`I command thee KNEEL `);
+  
