9f655e037bd820986472d25fb62bf96c6bb9c194
Talha <talha.b.future@gmail.com>
2022-06-14 18:39:23 +0300

    api works fine with catch

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,6 +1,17 @@
 import _ from 'lodash';
 
-
-
-  console.log(`I command thee KNEEL `);
+console.log(`I command thee Kneel`)
+const img = document.querySelector('img');
+  fetch('https://api.giphy.com/v1/gifs/translate?api_key=9YI8oJa8iE1iQVoOZrl0Lh0iffSffBPt&s=CATS', {mode: 'cors'})
+    .then(function(response) {
+        console.log(`number 1`)
+      return response.json();
+    })
+    .then(function(response) {
+        console.log(`number 2`) 
+        img.src = response.data.images.original.url;
+    })
+    .catch(function(response) {
+        console.log(`ERROR`)
+    })
   
