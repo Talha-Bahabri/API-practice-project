d98cd1ada0f4cf7516de464f987eb79b8bb2b5d4
Talha <talha.b.future@gmail.com>
2022-06-15 18:59:00 +0300

    trying search with word in search box

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -7,10 +7,20 @@ let refreshButton = document.getElementById("refreshButton")
 refreshButton.addEventListener("click" , displayNewImgRandomly )
 
   
-  
+let searchBox = document.getElementById("searchBox")
+let submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click" , function (e) {
+    console.log(`submit pressed`)
+    console.log(`${searchBox.value}`);
+    // return searchBox.valve;
+})
+
+
 
     function displayNewImgRandomly() {
-      fetch('https://api.giphy.com/v1/gifs/translate?api_key=9YI8oJa8iE1iQVoOZrl0Lh0iffSffBPt&s=CATS', {mode: 'cors'})
+      // fetch('https://api.giphy.com/v1/gifs/translate?api_key=9YI8oJa8iE1iQVoOZrl0Lh0iffSffBPt&s=CATS', {mode: 'cors'})
+      fetch(`https://api.giphy.com/v1/gifs/translate?api_key=9YI8oJa8iE1iQVoOZrl0Lh0iffSffBPt&s=${searchBox.value}`, {mode: 'cors'})
       .then(function(response) {
           console.log(`number 1`)
         return response.json();
