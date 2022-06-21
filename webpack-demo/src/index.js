7d5644d617bad0972ecb6dd17bcddaa882ce5002
Talha <talha.b.future@gmail.com>
2022-06-21 16:17:47 +0300

    removed the unnecesseray consol

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,6 +1,4 @@
-import _ from 'lodash';
-
-console.log(`I command thee Kneel`)
+import _ from 'lodash'; 
 const img = document.querySelector('img');
 
 let refreshButton = document.getElementById("refreshButton")
@@ -19,13 +17,12 @@ submitButton.addEventListener("click" , function (e) {
 
 
     function displayNewImgRandomly() {
-      // fetch('https://api.giphy.com/v1/gifs/translate?api_key=9YI8oJa8iE1iQVoOZrl0Lh0iffSffBPt&s=CATS', {mode: 'cors'})
       fetch(`https://api.giphy.com/v1/gifs/translate?api_key=9YI8oJa8iE1iQVoOZrl0Lh0iffSffBPt&s=${searchBox.value}`, {mode: 'cors'})
       .then(function(response) {
           console.log(`number 1`)
         return response.json();
       })
-      .then(function(response) {
+      .then(function(response) { 
           console.log(`number 2`) 
           img.src = response.data.images.original.url;
       })
